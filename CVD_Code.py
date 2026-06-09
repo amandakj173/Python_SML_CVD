2a75f89da235ef35bb0d487dca9f27ab566f732e
amanda173 <amandakj173@gmail.com>
2026-06-09 11:12:02 +0100

    feat: data preprocessing

--- CVD_Code.py
+++ CVD_Code.py
@@ -3,6 +3,7 @@
 # Import Packages
 import pandas as pd
 import numpy as np
+import random
 
 # Set Parameters
 pd.set_option("display.precision", 3)
@@ -36,3 +37,21 @@ Age_Sex_Desc = (Data_CVD.groupby('Sex')['Age']
            )
 print(Age_Sex_Desc)
 Data_CVD['HeartDisease'].value_counts(normalize = True)
+
+
+# PREPROCESSING
+
+# Randomisation
+random.seed(123)
+
+# Split Sets
+Observation = len(Data_CVD)  # Find number observations
+Test_IDX = np.random.choice(
+    Observation,
+    size = round(Observation * 0.3),
+    replace = False
+)  # Randomly select 30% row indices
+Test_Data = Data_CVD.iloc[Test_IDX]  # Attribute random 30% to test
+Train_Data = Data_CVD.drop(Data_CVD.index[Test_IDX])  # Drop random 30% and attribute rest to train
+print(Test_Data.info)  # Check no. test observations correct
+print(Train_Data.info)  # Check no. train observations correct
